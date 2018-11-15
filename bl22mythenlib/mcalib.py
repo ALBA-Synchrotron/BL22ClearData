f1d79b329282962556e4c7f589ff595964a0cc88
Roberto Javier Homs Puron <rhoms@cells.es>
2018-11-15 10:09:13 +0100

    Use global variables

--- bl22mythenlib/mcalib.py
+++ bl22mythenlib/mcalib.py
@@ -1,21 +1,21 @@
 import numpy as np
 from clear import Clear
-from utils import read_raw_data_spec
+from utils import read_raw_data_spec, CBRAGG, M_RAW, ENERGY, IO
 
 
-def mcalib(filename, scan_id, ftype='spec', threshold=0.7,
-           pixel_limit_noise=600):
+def mcalib(filename, scan_id, threshold=0.7,
+           pixel_limit_noise=600, ftype='spec'):
     clear = Clear()
 
     if ftype == 'spec':
-        data, snapshots = read_raw_data_spec(filename, scan_id)
+        data, snapshots = read_raw_data_spec(filename, [scan_id])[scan_id]
     else:
         raise ValueError('Only read spec files')
 
-    raw_mythen = np.array(data['m_raw'])
-    energies = np.array(data['energyc'])
-    i0 = np.array(data['n_i0_1'])
-    cbragg = snapshots['clear_bragg']
+    raw_mythen = np.array(data[M_RAW])
+    energies = np.array(data[ENERGY])
+    i0 = np.array(data[IO])
+    cbragg = snapshots[CBRAGG]
 
     # Normalize the data
     mythen_norm = raw_mythen/(i0[:, np.newaxis])*i0.mean()
