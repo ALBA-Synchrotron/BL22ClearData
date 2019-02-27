d387f7c4ba36ee64d746ec18242ce863c0ffb672
Roberto Javier Homs Puron <rhoms@cells.es>
2019-02-27 11:46:42 +0100

    Add get_energy_map method

--- bl22mythenlib/clear.py
+++ bl22mythenlib/clear.py
@@ -93,6 +93,7 @@ class Clear(object):
         self.a = self.k * self.mythen.fit_2d.a
         self.b = self.k * self.mythen.fit_2d.b + energies[0]
 
+        # TODO use the 2D dispersion instead of 1D
         # Use the 1D dispersion fitting to calculate the e0 and set the p0,
         # energy and pixel with maximum intensity
         self.p0 = self.mythen.fit_1d_pixel.p0
@@ -108,3 +109,101 @@ class Clear(object):
                                          self.k, self.order))
         self.log.debug('Exiting of elastic_line_calibration.')
 
+    def get_energy_map(self, ceout_data, mask=None):
+        '''
+        Method to get the energy functions coefficients for each scan point.
+
+        EnergyMap = A(p0-p) + B
+
+             B**2 * pixel_size
+        A =  ---------------------- * sqrt(B**2 - Chkl**2)
+                4 * R * Chkl**2
+
+        B = Ceout
+
+        R: Radius of the Roland Circle
+        Chkl: xxx
+
+        :param ceout_data: np.array(floats)
+        :return: [(A,B)]
+        '''
+
+        Chkl = self.crystal.getChkl(self.order)
+        self.log.debug('Chkl: {}'.format(Chkl))
+        R = self.rowland_rad
+        pixels_size = self.mythen.pixels_size
+        B = ceout_data.reshape((ceout_data.size, 1))
+        A = (B**2 * pixels_size * np.sqrt(B**2 - Chkl**2)) / (4 * R * Chkl**2)
+        self.log.debug('A: {}'.format(A))
+        self.log.debug('B: {}'.format(B))
+        p0 = self.p0
+        p = np.array(range(self.mythen.nr_pixels))
+        energy_map = A * (p0 - p) + B
+        self.log.debug('Emap: {}'.format(energy_map))
+        # TODO: Implement get_mask in mythen
+        if mask is None:
+            mask = self.mythen.mask
+        energy_map_masked = np.ma.masked_array(energy_map)
+        energy_map_masked.mask = mask
+        return energy_map_masked
+
+    def calc_mspectra(self, ceout_data, raw_mythen_data, dispersion_vector,
+                      dispersion_threshold=0.1, roi=None, remove_noise=True):
+
+        dispersion_vector = np.array(dispersion_vector)
+
+        # Prepare mask with the bad channels and the roi
+        bkp_roi = self.mythen.get_roi()
+        self.mythen.raw_data = raw_mythen_data
+        if roi is not None:
+            self.mythen.set_roi(roi)
+        mythen_mask = self.mythen.mask
+        mythen_data = self.mythen.data
+        if bkp_roi is None:
+            self.mythen.remove_roi()
+        else:
+            self.mythen.set_roi(bkp_roi)
+
+        # Concatenate the dispersion mask with the bad_channels and roi mask
+        dispersion_mask = (dispersion_vector < dispersion_threshold)
+        mask = mythen_mask & dispersion_mask
+        mythen_data.mask = mask
+
+        energy_map = self.get_energy_map(ceout_data, mask=mask)
+
+        if remove_noise:
+            mythen_data -= self.mythen.get_pixel_noise()
+
+        mythen_data *= dispersion_vector
+
+        intensity_vector = []
+
+        # TODO: ask why
+        emap = energy_map * 2
+
+        for idx, energy in enumerate(ceout_data):
+            if idx == 0:
+                e_min = energy
+            else:
+                e_min = ceout_data[idx - 1]
+            if idx == ceout_data.size - 1:
+                e_max = energy
+            else:
+                e_max = ceout_data[idx + 1]
+
+            e_idxs = (emap > (e_min + energy)) * (emap < (e_max + energy))
+            intensity_vector.append(mythen_data[e_idxs].sum())
+
+
+
+
+
+
+        # self.spectra = interp1d(ceout_real, Intensity, copy=False,
+        #                         bounds_error=False, fill_value=0.0)
+        #
+        # return noise, pixels, ceout_real, np.array(Ns), np.array(
+        #     NDisp_list), Emap.data, Disp, norm, np.array(
+        #     Intensity0), self.mythen.data[:,
+        #                  int(self.p0)] / self.mythen.dispersion_max, np.array(
+        #     Intensity)
