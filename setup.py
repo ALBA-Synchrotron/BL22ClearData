51a0068f9285b31df3268f7c6f3559ce5002ced7
Roberto Javier Homs Puron <rhoms@cells.es>
2019-10-16 11:28:56 +0200

    Bump version 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = '1.0.0'
+__version = '1.0.1'
 
 # windows installer:
 # python setup.py bdist_wininst
